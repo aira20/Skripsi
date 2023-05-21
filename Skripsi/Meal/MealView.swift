702461e81a3f1e4660e2f02a34b2a83cbb728c5f
Albertus Favian <favianalbert@gmail.com>
2023-05-21 17:59:35 +0700

    init tab meal

---
+++ Skripsi/Meal/MealView.swift
@@ -0,0 +1,23 @@
+//
+//  MealView.swift
+//  Skripsi
+//
+//  Created by Mac-albert on 21/05/23.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        VStack{
+            Text("Get Meal Plan")
+            Text("Choosen Meal Plan")
+        }
+    }
+}
+
+struct MealView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealView()
+    }
+}
