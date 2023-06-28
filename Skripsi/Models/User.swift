5c8f196dce0f0fb76e5f764972aea69014919b15
Albertus Favian <favianalbert@gmail.com>
2023-06-29 02:46:38 +0700

    fix loginRegister

--- Skripsi/Models/User.swift
+++ Skripsi/Models/User.swift
@@ -10,7 +10,10 @@ import FirebaseFirestoreSwift
 
 struct User: Codable{
     @DocumentID var id: String!
-    var name: String
+    var name: String?
+    var email: String?
+//    password seharusnya ga dimasukin ke firestore, tapi kalo mau ya terserah wkwk
+    var password: String?
     var time: Int?
     var frequency: Int?
 }
--- Skripsi/Register/RegisterViewModel.swift
+++ Skripsi/Register/RegisterViewModel.swift
@@ -10,113 +10,26 @@ import SwiftUI
 import FirebaseAuth
 import Firebase
 
-
-let authManager = AuthManager()
-
-//TODO: DON'T FORGET TO GET BACK TO THIS PROBLEM, NUMBER WHEEL PICKER
-
-struct MultiWheelPicker: UIViewRepresentable {
-    var selections: Binding<[Double]>
-    let data: [[Double]]
-    
-    func makeCoordinator() -> MultiWheelPicker.Coordinator {
-        Coordinator(self)
-    }
-    
-    func makeUIView(context: UIViewRepresentableContext<MultiWheelPicker>) -> UIPickerView {
-        let picker = UIPickerView()
-        picker.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        
-        picker.dataSource = context.coordinator
-        picker.delegate = context.coordinator
-        
-        return picker
-    }
-    
-    func updateUIView(_ view: UIPickerView, context: UIViewRepresentableContext<MultiWheelPicker>) {
-        for comp in selections.indices {
-            if let row = data[comp].firstIndex(of: selections.wrappedValue[comp]) {
-                view.selectRow(row, inComponent: comp, animated: false)
-            }
-        }
-    }
-    
-    class Coordinator: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
-        var parent: MultiWheelPicker
-      
-        init(_ pickerView: MultiWheelPicker) {
-            parent = pickerView
-        }
-        
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return parent.data.count
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return parent.data[component].count
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-            return 30
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            return String(format: "%02.0f", parent.data[component][row])
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-            parent.selections.wrappedValue[component] = parent.data[component][row]
-        }
-    }
-    
-}
-
-//====== For Saving data store in Firebase ======//
-
-class FirebaseManager {
-    func register(email: String, FullName: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                print("Registration failed: \(error.localizedDescription)")
-            } else {
-                completion(.success(()))
-            }
-        }
-    }
-    
-   
-    }
-
-
 class RegisterViewModel: ObservableObject {
     
     let authManager = AuthManager()
     
-    struct User
-    {
-        let username: String
-        let email: String
-        let password: String
-    }
-    
-    @Published var username = ""
+    @Published var fullName = ""
     @Published var email = ""
     @Published var password = ""
     @Published var confirmPassword = ""
     
     private var db = Firestore.firestore()
     
-    func register() {
-        guard isInputValid else { return }
+    func registerUser() {
+//        guard isInputValid else { return }
         
-        let user = User(username: username, email: email, password: password)
-        
-        //Data user once saved sends it directly to firebase
+        let user = User(name: fullName, email: email)
         
         db.collection("users").addDocument(data: [
-            "username": user.username,
+            "username": user.name,
             "email": user.email,
-            "password": user.password
+            "password": password
         ]) { error in
             if let error = error {
                 print("Error adding document: \(error)")
@@ -126,10 +39,20 @@ class RegisterViewModel: ObservableObject {
         }
     }
     
+    func register() {
+        authManager.registerUser(email: email, password: password, fullName: fullName) { (result, error) in
+            if let error = error {
+                print("Registration failed: \(error.localizedDescription)")
+            } else {
+                print("Registration successful!")
+            }
+        }
+    }
+    
     private var isInputValid: Bool {
         
         // Check if all fields are filled
-        guard !username.isEmpty, !email.isEmpty, !password.isEmpty, !confirmPassword.isEmpty else {
+        guard !fullName.isEmpty, !email.isEmpty, !password.isEmpty, !confirmPassword.isEmpty else {
             return false
         }
         
