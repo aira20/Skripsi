7cbbfe3e6d17fea55067b52f44fb68f93a3861f5
Albertus Favian <favianalbert@gmail.com>
2023-07-05 01:17:36 +0700

    hadiah buat ariak

--- Skripsi/Models/User.swift
+++ Skripsi/Models/User.swift
@@ -8,12 +8,13 @@
 import Foundation
 import FirebaseFirestoreSwift
 
-struct User: Codable{
-    @DocumentID var id: String!
+struct User: Codable {
+    @DocumentID var id: String?
     var name: String
     var password: String
     var email: String
     var accountCreated: Date?
     var time: Int?
     var frequency: Int?
+    var isAdmin = false
 }
